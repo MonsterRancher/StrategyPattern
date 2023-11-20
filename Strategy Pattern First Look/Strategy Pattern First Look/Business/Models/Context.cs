68c736ad59e6a7cfa8be361e18c6075d7bbb91a7
MonsterRancher <99233782+MonsterRancher@users.noreply.github.com>
2023-11-20 17:30:51 +0800

    Add files via upload

---
+++ Strategy Pattern First Look/Strategy Pattern First Look/Business/Models/Context.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Strategy_Pattern_First_Look.Business.Strategies;
+
+namespace Strategy_Pattern_First_Look.Business.Models
+{
+    public class Context
+    {
+        private IStrategy _strategy;
+
+        public Context(IStrategy strategy)
+        { 
+            this._strategy = strategy;
+        }
+
+        public Context()
+        {
+        }
+
+        public void SetStrategy(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        public void DoSomeBusinessLogic()
+        {
+            Console.WriteLine("Context: Sorting data using the strategy (not sure how it'll do it)");
+            var result = this._strategy.DoAlgorithm(new List<string> { "a", "b", "c", "d", "e" });
+
+            string resultStr = string.Empty;
+
+            foreach (var element in result as List<string>) {
+                resultStr += element + ",";
+            }
+
+            Console.WriteLine(resultStr);
+        }
+
+
+
+    }
+}
---
+++ Strategy Pattern First Look/Strategy Pattern First Look/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Strategy_Pattern_First_Look.Business.Models;
+using Strategy_Pattern_First_Look.Business.Strategies;
+
+class Program
+{ 
+
+    static void Main(string[] args) {
+
+        var context = new Context();
+
+        Console.WriteLine("Client: Strategy is set to normal sorting.");
+        context.SetStrategy(new ConcreteStrategyA());
+        context.DoSomeBusinessLogic();
+
+        Console.WriteLine();
+
+        Console.WriteLine("Client: Strategy is set to reverse sorting.");
+        context.SetStrategy(new ConcreteStrategyB());
+        context.DoSomeBusinessLogic();
+
+
+    }
+}
